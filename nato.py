687b428eace8bba491e26d747fda6d05651fea45
Imrane86 <bobot.imrane@student.emi.ac.ma>
2023-08-26 20:03:54 +0100

    Update nato.py
    it works now and i have been able to populate 2 dataframe

--- nato.py
+++ nato.py
@@ -10,55 +10,85 @@ import pandas as pd
 
 home_url = "https://manganato.com/"
 image_site = "https://chapmanganato.com/"
+latest_url =  "https://manganato.com/genre-all/"
 
-respense = requests.get(home_url)
-
-xml = respense.text
-soup = BeautifulSoup(xml,"html")
-
-manga_list = soup.find_all("div", class_="content-homepage-item")
 
 dict = {}
-for manga in manga_list:
-    name = manga.find("a")["title"]
-    url = (manga.find("a")["href"])
-    dict[name] = url
+# gathering manga list from top 50 pages
+for i in range(1,51):
+    # slow down the request
+    time.sleep(random.randint(1,3) + random.randint(1,9)*0.1)
+    # allow redirect
+    respense = requests.get(latest_url+str(i), allow_redirects=True)
+    # get the html
+    html = respense.text
+    # parse the html
+    soup = BeautifulSoup(html,"html")
+    # get the list of manga
+    manga_list = soup.find_all("div", class_="content-homepage-item")
+    # the dictionary is already created 
+    # TODO: make the dictionary a class
+    for manga in manga_list:
+        name = manga.find("a")["title"]
+        url = (manga.find("a")["href"])
+        dict[name] = url
+
+
+
 
 # create a pandas dataframe with colmns: name, cahpter, views, date.
 chapterData = pd.DataFrame(columns=["name","chapter","views","date"])
 # create a pandas dataframe for the manga: name, genre, author, status.
-mangaData = pd.DataFrame(columns=["name","genre","author","status"]) 
+mangaData = pd.DataFrame(columns=["name","genre","author","status","alt_title","views","score",'nb_votes']) 
 
+dictManga = {"name":"",
+             "genre":"",
+             "author":"",
+             "status":"",
+             "alt_title":"",
+             "views":"",
+             "score":"",
+             "nb_votes":""
+             }
+dictChapter = {"name":"",
+                "chapter":"",
+                "views":"",
+                "date":""
+                }
 for name, url in dict.items():
+    
     
     # get the manga page
     manga_page = requests.get(url)
     manga_soup = BeautifulSoup(manga_page.text,"html")
+    manga_info = manga_soup.find("table", class_="variations-tableInfo")
+    for name, url in dict.items():
+        # based on aug 26th 2023 
+        # get the manga page
+        manga_page = requests.get(url)
+        manga_soup = BeautifulSoup(manga_page.text,"html")
+        manga_info = manga_soup.find("table", class_="variations-tableInfo")
+        dictManga["name"] = name
+        dictManga["genre"] = manga_info.find_all("tr")[3].find("td", class_="table-value").text.strip().replace(" ","")
+        dictManga["author"] = manga_info.find_all("tr")[1].find("td", class_="table-value").text 
+        dictManga["status"] = manga_info.find_all("tr")[2].find("td", class_="table-value").text
+        dictManga["alt_title"] = manga_info.find_all("tr")[0].find("td", class_="table-value").text
+        dictManga["views"] = manga_soup.find_all("span",class_="stre-value")[1].text
+        dictManga["score"] = manga_soup.find_all("em")[7].text.strip()
+        dictManga["nb_votes"] = manga_soup.find_all("em")[9].text
+        mangaData = pd.concat([mangaData, pd.DataFrame([dictManga])], ignore_index=True)
+    # get the manga chapters
+
     list = manga_soup.find_all("li", class_="a-h") 
     # get the manga info
-    for item in list:
-        if item.find("span").text == "Genre":
-            genre = item.find("a").text
-        elif item.find("span").text == "Author(s)":
-            author = item.find("a").text
-        elif item.find("span").text == "Status":
-            status = item.find("a").text
-    # get the manga chapters
-    chapter_list = manga_soup.find_all("li", class_="a-h")
-    for chapter in chapter_list:
-        if chapter.find("a") != None:
-            chapter_name = chapter.find("a")["title"]
-            chapter_url = chapter.find("a")["href"]
-            chapter_views = chapter.find("span").text
-            chapter_date = chapter.find("i").text
-            chapterData = chapterData.append({"name":name,"chapter":chapter_name,"views":chapter_views,"date":chapter_date},ignore_index=True)
-    mangaData = mangaData.append({"name":name,"genre":genre,"author":author,"status":status},ignore_index=True) 
-    # get the manga cover
-    cover = manga_soup.find("div", class_="manga-info-pic")
-    cover_url = cover.find("img")["src"]
-    cover_name = name + ".jpg"
-
-    # 
+    for a in list:
+        # add to the dict
+        dictChapter["name"] = name
+        dictChapter["chapter"] = a.find('a', {'class': 'chapter-name'}).text
+        dictChapter["views"] = a.find('span', {'class': 'chapter-view'}).text
+        dictChapter["date"] = a.find('span', {'class': 'chapter-time'})["title"]
+        # add to the dataframe
+        chapterData = chapterData.append(dictChapter,ignore_index=True)
 
 
 
